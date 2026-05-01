ca85256371ed430abf301ca7e3efb3f3b47b5edc
stanleylew5 <stanley43595@gmail.com>
2026-05-01 02:09:27 -0700

    Add getWordle function to be used later

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,18 @@
-const Main = () => {
-  return <div>Testet</div>;
+/* async function getWordle(): Promise<string> {
+  const today = new Date().toISOString().slice(0, 10);
+
+  const res = await fetch(
+    `https://jindrich-bar--nyt-games-api.apify.actor/wordle/${today}?token=${process.env.APIFY_TOKEN}`,
+  );
+
+  const data = await res.json();
+  return data.word;
+} */
+
+const Main = async () => {
+  const word = "test";
+
+  return <div>Testing</div>;
 };
 
 export default Main;
